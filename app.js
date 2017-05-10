dc4af7c47dcf6020bdc066f800ba9c8083950be6
OJHA Ayush <ayojha@emea.msad.sopra>
2017-05-10 12:00:11 +0530

    Created package.json and app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: '42d4d3d7-d826-4830-b409-d40535fc4f54',
+    appPassword: 'wp0x51opQbTaO68tof3gAdp'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hey");
+});
